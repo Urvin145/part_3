9933ccfd448b8caf61e49dd17b7fcc9a08e60384
Urvin145 <109148939+Urvin145@users.noreply.github.com>
2022-09-14 23:30:57 +0530

    Add files via upload

---
+++ P3_5.java
@@ -0,0 +1,15 @@
+package pack1;
+
+     public class P3_5 {
+        public void me(){
+            System.out.println(" I am class A.");
+
+        }
+
+        public static void main(String[] args){
+            pack1.P3_5 obj = new pack1.P3_5();
+            obj.me();
+
+        }
+    }
+
